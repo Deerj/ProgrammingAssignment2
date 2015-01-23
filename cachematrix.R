bcddb3c781ea5f4d5a433547dcb0e6a79aa83859
Deerj <673479663@qq.com>
2015-01-23 13:16:18 +0800

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  inverse the invertible matrix by cache
 
-## Write a short comment describing this function
+## The functions in this module provide a convenient interface
+## to access and cache the inverse of a matrix
 
+
+## makeCacheMatrix -- creates a matrix object and 
+## its cache
 makeCacheMatrix <- function(x = matrix()) {
-
+        cacheinverse <- NULL
+        set <- function(newx){
+                x<<- newx
+                cacheinvers<<- NULL
+        }
+        get <- function() x
+        setcacheinverse <- function(inverse) cacheinverse <<- inverse
+        getcacheinverse <- function() cacheinverse
+        
+        list(set = set, get = get, setcaheinverse = setcaheinvese, getcacheinverse = getcaheinverse)
+        
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve -- returns the inverse  matrix stored in 
+## the previous function. And calls to cacheSolve() for
+## the  'makeCacheMatrix' result will return a cached inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cache <- x$getcacheinverse()
+        if(!is.null(cache)){
+                message("getting cached data")
+                retrurn(cache)
+        }
+        m <- x$get()
+        cahe <- solve(m)
+        x$setcacheinverse(cache)
+        cache
 }
